cbe7657bfdafd3046ffc099b3f456854e1fe388d
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-11-15 22:53:13 +1300

    Создан вспомогательный класс

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, surname, avatar, nickname, title, company_name, address, land_line, mobile, work, fax, email_1, email_2, email_3, homepage, birthday_day, birthday_month,
+                 birthday_year, anniversary_day, anniversary_month, anniversary_year, second_address, land_line_2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.surname = surname
+        self.avatar = avatar
+        self.nickname = nickname
+        self.title = title
+        self.company_name = company_name
+        self.address = address
+        self.land_line = land_line
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birthday_day = birthday_day
+        self.birthday_month = birthday_month
+        self.birthday_year = birthday_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.second_address = second_address
+        self.land_line_2 = land_line_2
+        self.notes = notes
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from contact import Contact
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -16,97 +17,94 @@ class test_add_contact(unittest.TestCase):
     def open_homepage(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, contact):
         # init contact creation
          wd.find_element_by_link_text("add new").click()
          # fill contact form
          wd.find_element_by_name("firstname").click()
          wd.find_element_by_name("firstname").clear()
-         wd.find_element_by_name("firstname").send_keys("Pavel")
+         wd.find_element_by_name("firstname").send_keys(contact.first_name)
          wd.find_element_by_name("middlename").click()
          wd.find_element_by_name("middlename").clear()
-         wd.find_element_by_name("middlename").send_keys("Ivanovich")
+         wd.find_element_by_name("middlename").send_keys(contact.middle_name)
          wd.find_element_by_name("lastname").click()
          wd.find_element_by_name("lastname").clear()
-         wd.find_element_by_name("lastname").send_keys("Kolosov")
-         wd.find_element_by_name("photo").send_keys("C:\\Users\\slaterr\\Pictures\\avatar.jpg")
+         wd.find_element_by_name("lastname").send_keys(contact.surname)
+         wd.find_element_by_name("photo").send_keys(contact.avatar)
          wd.find_element_by_name("nickname").click()
          wd.find_element_by_name("nickname").clear()
-         wd.find_element_by_name("nickname").send_keys("Kolos")
+         wd.find_element_by_name("nickname").send_keys(contact.nickname)
          wd.find_element_by_name("theform").click()
          wd.find_element_by_name("title").click()
          wd.find_element_by_name("title").clear()
-         wd.find_element_by_name("title").send_keys("Mr")
+         wd.find_element_by_name("title").send_keys(contact.title)
          wd.find_element_by_name("theform").click()
          wd.find_element_by_name("company").click()
          wd.find_element_by_name("company").clear()
-         wd.find_element_by_name("company").send_keys("Cobelco")
+         wd.find_element_by_name("company").send_keys(contact.company_name)
          wd.find_element_by_css_selector("body").click()
          wd.find_element_by_name("address").click()
          wd.find_element_by_name("address").clear()
-         wd.find_element_by_name("address").send_keys("85, Queen street, Auckland, New Zealand")
+         wd.find_element_by_name("address").send_keys(contact.address)
          wd.find_element_by_name("home").click()
          wd.find_element_by_name("home").clear()
-         wd.find_element_by_name("home").send_keys("+642223334455")
+         wd.find_element_by_name("home").send_keys(contact.land_line)
          wd.find_element_by_name("mobile").click()
          wd.find_element_by_name("mobile").clear()
-         wd.find_element_by_name("mobile").send_keys("+642223335556")
+         wd.find_element_by_name("mobile").send_keys(contact.mobile)
          wd.find_element_by_name("work").click()
          wd.find_element_by_name("work").clear()
-         wd.find_element_by_name("work").send_keys("+643334246622")
+         wd.find_element_by_name("work").send_keys(contact.work)
          wd.find_element_by_name("fax").click()
          wd.find_element_by_name("fax").clear()
-         wd.find_element_by_name("fax").send_keys("+644442221189")
+         wd.find_element_by_name("fax").send_keys(contact.fax)
          wd.find_element_by_name("email").click()
          wd.find_element_by_name("email").clear()
-         wd.find_element_by_name("email").send_keys("kolos@gmail.com")
+         wd.find_element_by_name("email").send_keys(contact.email_1)
          wd.find_element_by_name("theform").click()
          wd.find_element_by_name("email2").click()
          wd.find_element_by_name("email2").clear()
-         wd.find_element_by_name("email2").send_keys("kolos@yahoo.com")
+         wd.find_element_by_name("email2").send_keys(contact.email_2)
          wd.find_element_by_name("email3").click()
          wd.find_element_by_name("email3").clear()
-         wd.find_element_by_name("email3").send_keys("kolos@mail.ru")
+         wd.find_element_by_name("email3").send_keys(contact.email_3)
          wd.find_element_by_name("homepage").click()
          wd.find_element_by_name("homepage").clear()
-         wd.find_element_by_name("homepage").send_keys("http://kolos.info")
-         wd.find_element_by_name("homepage").click()
-         wd.find_element_by_name("homepage").clear()
-         wd.find_element_by_name("homepage").send_keys("http://kolos.info")
-         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").is_selected():
-                wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").click()
-         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").is_selected():
-                wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").click()
+         wd.find_element_by_name("homepage").send_keys(contact.homepage)
+         if not wd.find_element_by_xpath(contact.birthday_day).is_selected():
+                wd.find_element_by_xpath(contact.birthday_day).click()
+         if not wd.find_element_by_xpath(contact.birthday_month).is_selected():
+                wd.find_element_by_xpath(contact.birthday_month).click()
          wd.find_element_by_name("byear").click()
          wd.find_element_by_name("byear").clear()
-         wd.find_element_by_name("byear").send_keys("1980")
+         wd.find_element_by_name("byear").send_keys(contact.birthday_year)
          wd.find_element_by_name("theform").click()
-         if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").is_selected():
-                wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").click()
-         if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").is_selected():
-                wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").click()
+         if not wd.find_element_by_xpath(contact.anniversary_day).is_selected():
+                wd.find_element_by_xpath(contact.anniversary_day).click()
+         if not wd.find_element_by_xpath(contact.anniversary_month).is_selected():
+                wd.find_element_by_xpath(contact.anniversary_month).click()
          wd.find_element_by_name("ayear").click()
          wd.find_element_by_name("ayear").clear()
-         wd.find_element_by_name("ayear").send_keys("1999")
+         wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
          wd.find_element_by_name("theform").click()
          wd.find_element_by_name("address2").click()
          wd.find_element_by_name("address2").clear()
-         wd.find_element_by_name("address2").send_keys("30 Orakau Avenue, Epsom, Auckland, New Zealand ")
+         wd.find_element_by_name("address2").send_keys(contact.second_address)
          wd.find_element_by_name("phone2").click()
          wd.find_element_by_name("phone2").clear()
-         wd.find_element_by_name("phone2").send_keys("+645556667788")
+         wd.find_element_by_name("phone2").send_keys(contact.land_line_2)
          wd.find_element_by_name("notes").click()
          wd.find_element_by_name("notes").clear()
-         wd.find_element_by_name("notes").send_keys("Skype ID: kolos1980")
+         wd.find_element_by_name("notes").send_keys(contact.notes)
          # submit contact creation
          wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -116,8 +114,15 @@ class test_add_contact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_homepage(wd)
-        self.login(wd)
-        self.create_contact(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Contact(first_name="Pavel", middle_name="Ivanovich", surname="Kolosov", avatar="C:\\Users\\slaterr\\Pictures\\avatar.jpg", nickname="Kolos",
+                            title="Mr", company_name="Cobelco", address="85, Queen street, Auckland, New Zealand", land_line="+642223334455",
+                            mobile="+642223335556", work="+643334246622", fax="+644442221189", email_1="kolos@gmail.com", email_2="kolos@yahoo.com",
+                            email_3="kolos@mail.ru", homepage="http://kolos.info", birthday_day="//div[@id='content']/form/select[1]//option[6]",
+                            birthday_month="//div[@id='content']/form/select[2]//option[7]", birthday_year="1980",
+                            anniversary_day="//div[@id='content']/form/select[3]//option[4]",
+                            anniversary_month="//div[@id='content']/form/select[4]//option[2]", anniversary_year="1999",
+                            second_address="30 Orakau Avenue, Epsom, Auckland, New Zealand ", land_line_2="+645556667788", notes="Skype ID: kolos1980"))
         self.logout(wd)
 
     def tearDown(self):
