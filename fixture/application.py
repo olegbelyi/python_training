7e461407f69553dbcaade5ac34590284c068cf3b
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-11-20 21:23:21 +1300

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
