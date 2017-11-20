633b6820705acb40ce986b94ca8235cbbb2e323d
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-11-20 22:05:40 +1300

    Выделен класс для работы с контактами, удалена фикстура 'entry', которая отдельно использовалась для работы с контактами.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,27 +1,10 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-class Entry:
+class ContactHelper:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+    def __init__(self, app):
+        self.app = app
 
-    def open_homepage(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_homepage()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
+    def create(self, contact):
+        wd = self.app.wd
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
@@ -102,10 +85,3 @@ class Entry:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def destroy(self):
-        self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from fixture.entry import Entry
+from fixture.application import Application
 from model.contact import Contact
 
-
 @pytest.fixture
-def ent(request):
-    fixture = Entry()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_contact(ent):
-        ent.login(username="admin", password="secret")
-        ent.create_contact(Contact(first_name="Pavel", middle_name="Ivanovich", surname="Kolosov", avatar="C:\\Users\\slaterr\\Pictures\\avatar.jpg", nickname="Kolos",
-                            title="Mr", company_name="Cobelco", address="85, Queen street, Auckland, New Zealand", land_line="+642223334455",
-                            mobile="+642223335556", work="+643334246622", fax="+644442221189", email_1="kolos@gmail.com", email_2="kolos@yahoo.com",
-                            email_3="kolos@mail.ru", homepage="http://kolos.info", birthday_day="//div[@id='content']/form/select[1]//option[6]",
-                            birthday_month="//div[@id='content']/form/select[2]//option[7]", birthday_year="1980",
-                            anniversary_day="//div[@id='content']/form/select[3]//option[4]",
-                            anniversary_month="//div[@id='content']/form/select[4]//option[2]", anniversary_year="1999",
-                            second_address="30 Orakau Avenue, Epsom, Auckland, New Zealand ", land_line_2="+645556667788", notes="Skype ID: kolos1980"))
-        ent.logout()
+def test_add_contact(app):
+        app.session.login(username="admin", password="secret")
+        app.contact.create(Contact(first_name="Pavel", middle_name="Ivanovich", surname="Kolosov", avatar="C:\\Users\\slaterr\\Pictures\\avatar.jpg", nickname="Kolos",
+                                   title="Mr", company_name="Cobelco", address="85, Queen street, Auckland, New Zealand", land_line="+642223334455",
+                                   mobile="+642223335556", work="+643334246622", fax="+644442221189", email_1="kolos@gmail.com", email_2="kolos@yahoo.com",
+                                   email_3="kolos@mail.ru", homepage="http://kolos.info", birthday_day="//div[@id='content']/form/select[1]//option[6]",
+                                   birthday_month="//div[@id='content']/form/select[2]//option[7]", birthday_year="1980",
+                                   anniversary_day="//div[@id='content']/form/select[3]//option[4]",
+                                   anniversary_month="//div[@id='content']/form/select[4]//option[2]", anniversary_year="1999",
+                                   second_address="30 Orakau Avenue, Epsom, Auckland, New Zealand ", land_line_2="+645556667788", notes="Skype ID: kolos1980"))
+        app.session.logout()
 
 
