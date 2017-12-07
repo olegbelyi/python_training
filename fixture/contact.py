20d12d4ee0c1a27508fa6b50ecb52e9fdfea8fc3
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-12-07 20:17:00 +1300

    Оптимизированы переходы между страницами(добавлены проверки открытия нужной страницы)

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,7 +5,8 @@ class ContactHelper:
 
     def open_homepage(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
