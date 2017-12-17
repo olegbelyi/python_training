180dc3b29d66d873ba722daf73f5260fc940270c
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-12-17 22:30:38 +1300

    [Доработан] Тест сравнения информации контакта на страницах редактирования и на главной странице

--- test/test_contact_assertion.py
+++ test/test_contact_assertion.py
@@ -1,18 +1,20 @@
 from random import randrange
 import re
 
+
 def test_contact_info_assertion_on_edit_page(app):
     index = randrange(len(app.contact.get_contact_list()))
     app.contact.select_contact_by_index(index)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
-    assert contact_from_home_page.surname == contact_from_edit_page.surname
+    assert clear(contact_from_home_page.first_name) == clear(contact_from_edit_page.first_name)
+    assert clear(contact_from_home_page.surname) == clear(contact_from_edit_page.surname)
+    assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def clear(s):
-    return re.sub("[() -]", "", s)
+    return re.sub("[() -'\n']", "", s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
