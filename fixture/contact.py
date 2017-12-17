75d65e686af64a844cc5f812ea46055aabdb41b0
olegbelyi <32450263+olegbelyi@users.noreply.github.com>
2017-12-17 18:45:06 +1300

    Добавлен тест сравнения информации на главной странице и на странице редактирования для случайно выбранного контакта.

--- fixture/contact.py
+++ fixture/contact.py
@@ -172,11 +172,13 @@ class ContactHelper:
                 cells = row.find_elements_by_tag_name("td")
                 first_name = cells[2].text
                 surname = cells[1].text
-                #print (first_name + " " + surname)
+                address = cells[3].text
+                all_emails = cells[4].text
                 id = cells[0].find_element_by_name("selected[]").get_attribute("value")
                 all_phones = cells[5].text
                 self.contact_cache.append(Contact(first_name=first_name, surname=surname, id=id,
-                                                  all_phones_from_home_page=all_phones))
+                                                  all_phones_from_home_page=all_phones, address=address,
+                                                  all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -198,13 +200,18 @@ class ContactHelper:
         self.open_contact_to_edit_by_index(index)
         first_name = wd.find_element_by_name("firstname").get_attribute("value")
         surname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
         land_line = wd.find_element_by_name("home").get_attribute("value")
         mobile = wd.find_element_by_name("mobile").get_attribute("value")
         work = wd.find_element_by_name("work").get_attribute("value")
         land_line_2 = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(first_name=first_name, surname=surname, id=id,
-                       land_line=land_line, mobile=mobile, work=work, land_line_2=land_line_2)
+        email_1 = wd.find_element_by_name("email").get_attribute("value")
+        email_2 = wd.find_element_by_name("email2").get_attribute("value")
+        email_3 = wd.find_element_by_name("email3").get_attribute("value")
+        return Contact(first_name=first_name, surname=surname, address=address, id=id,
+                       land_line=land_line, mobile=mobile, work=work, land_line_2=land_line_2,
+                       email_1=email_1, email_2=email_2, email_3=email_3 )
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,7 @@ class Contact:
     def __init__(self, first_name=None, middle_name=None, surname=None, id=None,
                  avatar=None, nickname=None, title=None, company_name=None, address=None,
                  land_line=None, mobile=None, work=None, land_line_2=None, all_phones_from_home_page=None, fax=None,
-                 email_1=None, email_2=None, email_3=None,
+                 email_1=None, email_2=None, email_3=None, all_emails_from_home_page=None,
                  homepage=None, birthday_day=None, birthday_month=None, birthday_year=None,
                  anniversary_day=None, anniversary_month=None, anniversary_year=None, second_address=None, notes=None):
         self.first_name = first_name
@@ -25,6 +25,7 @@ class Contact:
         self.email_1 = email_1
         self.email_2 = email_2
         self.email_3 = email_3
+        self.all_emails_from_home_page = all_emails_from_home_page
         self.homepage = homepage
         self.birthday_day = birthday_day
         self.birthday_month = birthday_month
